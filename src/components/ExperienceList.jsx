f360c37a50624c62c1cf068fd6016e8a313cc723
Leila <leila.benhalima2@gmail.com>
2024-06-14 10:46:01 +0200

    fix space

--- src/components/ExperienceList.jsx
+++ src/components/ExperienceList.jsx
@@ -47,7 +47,7 @@ export default function ExperienceList(props) {
         })
       ) : (
         <div>
-          <h3 className="mt-4" onClick={addExperience}>
+          <h3 className="mt-4 md:mx-48 " onClick={addExperience}>
             <button className="md:mt-4 inline-flex relative w-fit md:w-22 items-center rounded-md bg-primary px-3 py-2 text-lg font-semibold text-gray-800 shadow-sm ring-1 ring-inset ring-primary hover:bg-[#f39b0c]">
               <PlusIcon className="h-5 w-5" />
               Add experience
