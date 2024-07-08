1d3e3d417d1a4d37b19d50a6e3ab56af7d35f68e
Leila <leila.benhalima2@gmail.com>
2024-07-08 13:10:44 +0200

    fix screenshot function

--- src/utils/htmltoImage.jsx
+++ src/utils/htmltoImage.jsx
@@ -1,28 +1,15 @@
-import html2canvas from "html2canvas";
+import * as htmlToImage from "html-to-image";
+import download from "downloadjs";
 
 export default function screenShot() {
-  html2canvas(document.body, {
-    useCORS: true,
-  }).then(function (canvas) {
-    // Convert the canvas to a data URL
-    var dataURL = canvas.toDataURL("image/png");
-
-    // Create a link element
-    var downloadLink = document.createElement("a");
-
-    // Set the download attribute with a filename
-    downloadLink.download = "myResume.png";
-
-    // Set the href of the link to the data URL
-    downloadLink.href = dataURL;
-
-    // Append the link to the document
-    document.body.appendChild(downloadLink);
-
-    // Programmatically click the link to trigger the download
-    downloadLink.click();
-
-    // Remove the link from the document
-    document.body.removeChild(downloadLink);
-  });
+  htmlToImage
+    .toPng(document.body)
+    .then(function (dataUrl) {
+      var img = new Image();
+      img.src = dataUrl;
+      download(dataUrl, "my-resume.png");
+    })
+    .catch(function (error) {
+      console.error("oops, something went wrong!", error);
+    });
 }
